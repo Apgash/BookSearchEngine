edf6a8bacafef055c09f15c1ae9ebafe957c35ef
Apgash <ashtonvilkosi@gmail.com>
2025-01-07 21:04:30 -0500

    updates app.tsx

--- booksearch/Develop/client/src/App.tsx
+++ booksearch/Develop/client/src/App.tsx
@@ -8,7 +8,26 @@ function App() {
     <>
       <Navbar />
       <Outlet />
-    </>
+    </>import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Navbar />
+      <Outlet />
+    </ApolloProvider>
+  );
+}
+
+export default App;
   );
 }
 
